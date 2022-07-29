6708f2b5e7c235f106a523e4b76172acc7912ac7
Ana <ana@email.com>
2022-07-29 07:16:06 -0300

    Criando os Controlles de Sales e Vendors bem como suas Models e já configurando previamente algumas rotas que poderão ser utilizadas no desenvolvimento.

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    public function index ()
+    {
+
+    }
+
+    public function create ()
+    {
+
+    }
+
+    public function store ()
+    {
+
+    }
+
+    public function update ()
+    {
+
+    }
+
+    public function delete ()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\SalesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendorsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/vendors', VendorsController::class)
+    ->except(['show']);
+
+Route::get('/sale', [SalesController::class, 'index'])->name('sales.index');
