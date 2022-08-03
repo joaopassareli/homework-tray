a3bec7c9940ed2f9e57a5a4ad1b2862afd243537
Joao Passareli <ana@email.com>
2022-08-03 08:00:20 -0300

    Corrigindo um bug de importação do SalesReportController.

--- app/Http/Controllers/SalesReportController.php
+++ app/Http/Controllers/SalesReportController.php
@@ -13,21 +13,10 @@ class SalesReportController extends Controller
     {
         $vendor = Vendor::find($id);
         $sales = Sale::whereVendor_id($vendor->id)->get();
-        $sales1 = Sale::all();
-        $vendors = Vendor::all();
 
         $totalComission = CalculateComission::calculateComission($sales);
 
-        foreach ($sales1 as $sale) {
-            DailySalesReportEvent::dispatch(
-                $sale->id,
-                $sale->sale_value,
-                $sale->created_at,
-                $vendors->find($sale->vendor_id)->name,
-            );
-        }
-
-        return view('sales-report.show')
+                return view('sales-report.show')
             ->with('vendor', $vendor)
             ->with('sales', $sales)
             ->with('totalComission', $totalComission);
