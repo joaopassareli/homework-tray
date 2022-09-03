c8cfa45cdf4a50e500381063cfd8e3dd4cd22ce7
João Passareli <joaopassareli@gmail.com>
2022-09-03 17:50:45 -0300

    Criado todos os testes unitários para as models Sale e Vendors, garantindo que as opções de criação, edição e exclusão de Vendor está funcionando, bem como a criação de Sale

--- tests/Feature/SaleControllerTest.php
+++ tests/Feature/SaleControllerTest.php
@@ -2,21 +2,50 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Vendor;
+use App\Repository\SaleRepository;
+use App\Repository\VendorRepository;
+use App\Http\Requests\SalesFormRequest;
+use App\Http\Requests\VendorsFormRequest;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SaleControllerTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+    use WithFaker;
 
-        $response->assertStatus(302);
+    public function testConfirmThatSaleIsCreated()
+    {
+        $vendor = $this->createVendor();
+        //Arrange
+        /** @var SaleRepository $repository */
+        $saleRepository = $this->app->make(SaleRepository::class);
+
+        $request = new SalesFormRequest();
+        $request->sale_value = 3000.00;
+        $request->vendor_id = 1;
+
+        //Act
+        $saleRepository->add($request->sale_value, $request->vendor_id);
+
+        //Assert
+        $this->assertDatabaseHas('sales', [
+            'sale_value' => 3000,
+            'vendor_id' => 1,
+        ]);
+    }
+
+    private function createVendor()
+    {
+        /** @var VendorRepository $repository */
+        $vendorRepository = $this->app->make(VendorRepository::class);
+
+        $request = new VendorsFormRequest();
+        $request->name = 'João Passareli';
+        $request->email = 'joaopassareli@gmail.com';
+
+        return $vendorRepository->add($request->name, $request->email);
     }
 }
--- tests/Feature/VendorControllerTest.php
+++ tests/Feature/VendorControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Vendor;
 use App\Repository\VendorRepository;
 use App\Http\Requests\VendorsFormRequest;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,7 +27,55 @@ class VendorControllerTest extends TestCase
         $vendorRepository->add($request->name, $request->email);
 
         //Assert
-        $this->assertDatabaseHas('vendors', ['name' => 'João Passareli']);
-        $this->assertDatabaseHas('vendors', ['email' => 'joaopassareli@gmail.com']);
+        $this->assertDatabaseHas('vendors', [
+            'name' => 'João Passareli',
+            'email' => 'joaopassareli@gmail.com'
+        ]);
+        // $this->assertDatabaseHas('vendors', ['email' => 'joaopassareli@gmail.com']);
+    }
+
+    public function testConfirmThatVendorIsEdited ()
+    {
+        //Arrange
+        /** @var VendorRepository $repository */
+        $vendorRepository = $this->app->make(VendorRepository::class);
+
+        $request = new VendorsFormRequest();
+        $request->name = 'João Passareli';
+        $request->email = 'joaopassareli@gmail.com';
+
+        // Updating the data from Vendor
+        $vendor = new Vendor();
+        $vendor->name = 'João Victor Passareli';
+        $vendor->email = 'joao.passareli@gmail.com';
+
+        //Act
+        $vendorRepository->add($request->name, $request->email);
+        $vendorRepository->update($vendor, $request);
+
+
+        //Assert
+        $this->assertDatabaseHas('vendors', [
+            'name' => 'João Victor Passareli',
+            'email' => 'joao.passareli@gmail.com'
+        ]);
+    }
+
+    public function testConfirmThatVendorWasDeleted ()
+    {
+        //Arrange
+        /** @var VendorRepository $repository */
+        $vendorRepository = $this->app->make(VendorRepository::class);
+
+        $request = new VendorsFormRequest();
+        $request->name = 'João Passareli';
+        $request->email = 'joaopassareli@gmail.com';
+
+        //Act
+        $vendor = $vendorRepository->add($request->name, $request->email);
+        $vendorRepository->destroy($vendor);
+
+        // Assert
+        $this->assertDatabaseMissing('vendors', ['name' => 'João Passareli']);
     }
 }
